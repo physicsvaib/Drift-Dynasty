9c5f5b870cb37fa0a046d9bac8797a8dc7543fd5
physicsvaib <31775546+physicsvaib@users.noreply.github.com>
2024-03-25 10:46:04 +0530

    Commit #2| Assets/Drift Dynasty/Prefabs.meta Assets/Drift Dynasty/Prefabs/Car.prefab Assets/Drift Dynasty/Prefabs/Car.prefab.meta Assets/Drift Dynasty/Prefabs/SphereCar.prefab Assets/Drift Dynasty/Prefabs/SphereCar.prefab.meta Assets/Drift Dynasty/Scenes/Sample1.unity Assets/Drift Dynasty/Scenes/Sample2.unity Assets/Drift Dynasty/Scripts/Car/SphereMovement.cs ProjectSettings/DynamicsManager.asset ProjectSettings/TagManager.asset

--- Assets/Drift Dynasty/Scripts/Car/SphereMovement.cs
+++ Assets/Drift Dynasty/Scripts/Car/SphereMovement.cs
@@ -7,30 +7,52 @@ namespace Phyw.Car
   public class SphereMovement : MonoBehaviour
   {
     #region Variables
-    [SerializeField] private Rigidbody rb;
-    [SerializeField] private float speed = 5;
-    [SerializeField] private float rotSpeed = 5;
+    [SerializeField] private Rigidbody sphere;
+    [SerializeField] Transform car;
+    [SerializeField] Transform normal;
+    [SerializeField] private float rotSpeed = 1.0f;
+    [SerializeField] private float lerpSpeed = 1.0f;
+    [SerializeField] private float speed = 1.0f;
+    [SerializeField] LayerMask maskGround;
 
-    [SerializeField] private Grounded grounded;
-
+    private Vector3 offset;
     private Vector2 vec;
+    private RaycastHit hit;
+
     private bool isPressingMove;
     #endregion
 
-    #region UnityMethods
-    private void Update()
+    private void Start()
     {
-      if (isPressingMove)
-      {
-        // Check if grounded then add full force 
-        if (grounded.GetGrounded())
-        {
-          rb.AddForce(rb.transform.forward * vec.y * Time.deltaTime * 100 * speed);
-        }
-        transform.Rotate(0, vec.x * rotSpeed * Time.deltaTime, 0);
-      }
+      offset = sphere.position - car.position;
     }
 
+    #region UnityMethods
+
+
+    private void FixedUpdate()
+    {
+      // Gravity
+      sphere.AddForce(Vector3.down * 30, ForceMode.Acceleration);
+
+      Vector3 start = car.position;
+      Physics.Raycast(start, Vector3.down, out hit, 1, maskGround);
+      if (hit.collider)
+      {
+        //Check if grounded then add full force
+        normal.up = Vector3.Lerp(normal.up, hit.normal, lerpSpeed);
+        //normal.up = hit.normal;
+        //normal.Rotate(0, transform.eulerAngles.y, 0);
+        print($"The collider is there {hit.collider.name}");
+        sphere.AddForce(car.transform.forward * vec.y * 100 * speed, ForceMode.Impulse);
+      }
+      car.Rotate(0, vec.x * rotSpeed, 0);
+      car.position = sphere.position - offset;
+    }
+    private void Update()
+    {
+
+    }
     #endregion
 
     #region CustomMethods
@@ -52,3 +74,14 @@ namespace Phyw.Car
 
   }
 }
+
+
+/*
+
+  * Sphere will handle collisions
+    
+  * Detect the normal postion to get the car forward
+    car.up = normal 
+ * Move towards the car forward
+    sphere.addForce(car.forward) 
+*/
